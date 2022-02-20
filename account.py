0e1b74665619cb590f9f738fb14e06f23e07c2d3
Zappiee <tanzijian2@gmail.com>
2022-02-21 02:17:09 +0800

    Update account.py

--- account.py
+++ account.py
@@ -4,6 +4,9 @@ from tkinter.ttk import Label
 import pygame as pg
 from email import message
 from tkinter import Image, PhotoImage, messagebox
+from PIL import ImageTk, Image
+# PIL is to allow to save and display other image file formats cus rn only png works
+
 
 # Import Local Module
 from py_SQL import db_connection
@@ -31,10 +34,15 @@ root.geometry("500x500")
 root.resizable(width=False, height=False)
 root.title('User Profile')
 
-#display image from database
+#display image
 mylabel = Label(root, text="bye world!")
 mylabel.pack()
 photo = PhotoImage(file="C:\\Users\\USER\\Desktop\\kanna3.png")
 labeling = Label(root, image=photo)
 labeling.pack()
+
+# converting jpg images so it will appear in in the application
+img = ImageTk.PhotoImage(Image.open("xyz.jpg"))  
+l=Label(image=img)
+l.pack()
 root.mainloop()
